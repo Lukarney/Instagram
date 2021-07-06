a81c0167bc5861687876bb34ed6d7797db82f378
Luke Arney <57237540+Lukarney@users.noreply.github.com>
2021-07-06 14:42:44 -0500

    configured parse server with Back4App

--- Instagram/AppDelegate.m
+++ Instagram/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
 
 @interface AppDelegate ()
 
@@ -16,6 +17,16 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+
+            configuration.applicationId = @"S9Aa7jYFaW5ZE9MD1G19FCYbLti4VeqGtqrRbR0G"; // <- UPDATE
+            configuration.clientKey = @"Z9sG6nrnMTg2SM4JYkAZGqRH3gzEUXs2iXCDPswZ"; // <- UPDATE
+            configuration.server = @"https://parseapi.back4app.com";
+        }];
+
+        [Parse initializeWithConfiguration:config];
+    
+    
     return YES;
 }
 
