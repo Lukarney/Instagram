11f66ea6b996bb009aeaa74b1855b4039df51b0d
Luke Arney <57237540+Lukarney@users.noreply.github.com>
2021-07-07 16:20:27 -0500

    log out and user persistence

--- Instagram/View Controllers/LoginViewController.m
+++ Instagram/View Controllers/LoginViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "LoginViewController.h"
+#import <Parse/Parse.h>
 
 @interface LoginViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *usernameField;
@@ -21,42 +22,51 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
-- (IBAction)signupPressed:(UIButton *)sender {
-    // initialize a user object
-        PFUser *newUser = [PFUser user];
-        
-        // set user properties
-        newUser.username = self.usernameField.text;
-        newUser.password = self.passwordField.text;
-        
-        // call sign up function on the object
-        [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
-            if (error != nil) {
-                NSLog(@"Error: %@", error.localizedDescription);
-            } else {
-                NSLog(@"User registered successfully");
-                
-                // manually segue to logged in view
-                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
-            }
-        }];
+- (IBAction)loginPressed:(id)sender {
+    [self loginUser];
 }
-- (IBAction)loginPressed:(UIButton *)sender {
-    NSString *username = self.usernameField.text;
-        NSString *password = self.passwordField.text;
-        
-        [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
-            if (error != nil) {
-                NSLog(@"User log in failed: %@", error.localizedDescription);
-            } else {
-                NSLog(@"User logged in successfully");
-                
-                // display view controller that needs to shown after successful login
-                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
-            }
-        }];
+- (IBAction)signupPressed:(id)sender {
+    [self signupUser];
 }
 
+
+
+-(void)signupUser {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
+}
+
+-(void)loginUser {
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+        
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            
+            // display view controller that needs to shown after successful login
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
+}
 /*
 #pragma mark - Navigation
 
